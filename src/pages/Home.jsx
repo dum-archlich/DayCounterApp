6cd6df3530b9e67c374db88045fbd01c009a998b
MakdumIbrohim <makdumibrohim3@gmail.com>
2025-12-06 18:43:43 +0700

    style(ui): adjust layout and add title in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,11 @@ import DatePicker from "../components/DateRangePicker";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-v w-[80%] h-auto bg-blue-500 text-white p-6 text-center absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3">
-      <div className="text-left">
-        <p className="mb-4">
-          Select a date range to calculate the number of days between two dates.
+    <div className="w-[80%] max-w-lg bg-blue-500 text-white p-8 rounded-lg shadow-lg absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/4.5">
+      <div className="text-center">
+        <h3 className="text-xl font-bold mb-4">Day Counter App</h3>
+        <p className="mb-6">
+          Select a date range to calculate the duration between two dates.
         </p>
         <DatePicker />
       </div>
